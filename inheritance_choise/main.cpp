3d85a00e7ea69e3aa37bd1e1c94b520b3edbb04f
Vladimir <nekromant_king@mail.ru>
2015-10-13 09:42:35 +0300

    Timer

--- inheritance_choise/main.cpp
+++ inheritance_choise/main.cpp
@@ -1,17 +1,29 @@
 #include <QDebug>
 #include <calculation.h>
 #include <form.h>
+#include <conio.h>
+#include <QTcpSocket>
+#include <QTimer>
+#include <QObject>
+#include <QCoreApplication>
 
-int main()
+int main(int argc, char **argv)
 {
-
+    QCoreApplication app(argc, argv); //цикл обработки событий Qt
     Calculation calc;
     Form form;
     QObject::connect(&calc, &Calculation::test, &form, &Form::onTest);
     emit calc.test();
 
+    QTimer::singleShot(500, &form, &Form::onTest);  //статическая функция
 
-    return 0;
+
+    QTimer* ptimer = new QTimer(&form);
+    QObject::connect(ptimer, &QTimer::timeout,  &form, &Form::onTest); //использовать этот синтаксис
+    ptimer->start(1000);
+    //ptimer->singleShot(true);
+
+    return app.exec();
 }
 
 
